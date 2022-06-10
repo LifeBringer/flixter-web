92a8d7d30f543aab22c60da5d7a967927ace0693
LifeBringer <LifeBringer@users.noreply.github.com>
2022-06-10 16:53:15 -0400

    added persistent header

--- script.js
+++ script.js
@@ -187,6 +187,24 @@ async function handleSearchFormSubmit(event) {
     closeSearchBtn.classList.remove('hidden');
 }
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+const navbar = document.querySelector("header");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
 function setupEvents() {
     searchForm.addEventListener('submit', handleSearchFormSubmit);
 }
